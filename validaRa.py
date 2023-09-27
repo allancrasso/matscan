f5078f84e97210a66908ab9c0c8ed71e136d5e31
Allan Crasso <60273934+allancrasso@users.noreply.github.com>
2023-09-27 12:22:02 -0300

    Add files via upload

--- validaRa.py
+++ validaRa.py
@@ -19,13 +19,20 @@ def validar_ra(ra, arquivo_xlsx):
     return False
 
 def verificar_ra():
-    ra = ra_entry.get()
+    ra = ra_entry.get().strip()  # Remove espaços em branco do início e do final
+    ra_entry.delete(0, tk.END)   # Limpa a entrada
     if ra.lower() == "exit":
         root.quit()
     elif validar_ra(ra, arquivo_xlsx):
         messagebox.showinfo("Resultado", "RA validado com sucesso!")
     else:
         messagebox.showerror("Resultado", "RA inválido!")
+
+def ativar_verificacao_automatica():
+    ra = ra_entry.get().strip()  # Remove espaços em branco do início e do final
+    if ra:  # Verifica se o campo não está vazio
+        verificar_ra()  # Chama a função verificar_ra() se o campo não estiver vazio
+    root.after(500, ativar_verificacao_automatica)  # Agenda a próxima chamada após 0,5 segundos
 
 # Nome do arquivo XLSX com os RAs válidos (sem a extensão)
 arquivo_xlsx = "Ras"
@@ -44,5 +51,11 @@ ra_entry.pack()
 verificar_button = tk.Button(root, text="Verificar", command=verificar_ra)
 verificar_button.pack()
 
+# Ativar verificação automática após 0,5 segundos
+root.after(500, ativar_verificacao_automatica)
+
 root.mainloop()
+
+
+
 
